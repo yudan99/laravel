b83dc0c14082b6689c34ea11cc0a4ab9fde0de4f
vagrant <vagrant@homestead>
2020-11-09 04:16:44 +0000

    增加字体图标// Fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', '') - 欢迎来到凑学</title>
+    <title>@yield('title', '') 欢迎来到凑学</title>
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
