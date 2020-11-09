1a14ddad024e41bfab03a2f67be8c72b9e50ba20
vagrant <vagrant@homestead>
2020-11-09 08:12:25 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -29,12 +29,15 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:200',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=200',
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '头像不够清晰哦，头像宽和高，请在200像素或以上',
             'name.unique' => '用户名已被占用，请重新填写',
             'name.regex' => '用户名只支持英文、数字、横杠和下划线',
             'name.between' => '用户名必须介于 3 - 25 个英文之间',
