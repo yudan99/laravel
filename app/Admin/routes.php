e361858fc122f0c7970bf367b972c69fe71e28b3
vagrant <vagrant@homestead>
2020-11-21 12:53:02 +0000

    领域类目管理后台

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,12 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('users', 'UsersController@index');
 
+    $router->get('fiels', 'FielsController@index');
+    $router->get('fiels/create', 'FielsController@create');
+    $router->get('fiels/{id}/edit', 'FielsController@edit');
+    $router->post('fiels', 'FielsController@store');
+    $router->put('fiels/{id}', 'FielsController@update');
+    $router->delete('fiels/{id}', 'FielsController@destroy');
+    $router->get('api/fiels', 'FielsController@apiIndex');
+
 });
