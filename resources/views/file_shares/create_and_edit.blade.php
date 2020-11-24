dd4d9f8f3fae6d0337acea03eae6839ba31bbd4e
vagrant <vagrant@homestead>
2020-11-24 09:04:38 +0000

    解决了quil前端无法提交数据的bug

--- resources/views/file_shares/create_and_edit.blade.php
+++ resources/views/file_shares/create_and_edit.blade.php
@@ -19,10 +19,10 @@
       <div class="card-body">
 
         @if($file_share->id)
-          <form action="{{ route('file_shares.update', $file_share->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
+          <form action="{{ route('file_shares.update', $file_share->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data" pjax-container>
           <input type="hidden" name="_method" value="PUT">
         @else
-          <form action="{{ route('file_shares.store') }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
+          <form action="{{ route('file_shares.store') }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data" pjax-container>
         @endif
 
           @include('common.error')
@@ -42,14 +42,15 @@
               </div>
 
 
-              <div class="form-group"  contenteditable="true" >
-                  <div  contenteditable="true"  name="file_introduction" class="form-control" id="editor" style="height: 500px;overflow:auto" placeholder="请填入至少三个字的内容" required>
-                      {{ old('file_introduction', $file_share->file_introduction ) }}
+              <div class="form-group" >
+                  <div id="file_introduction" style="height: 200px">
+                      {!! old('file_introduction', $file_share->file_introduction ) !!}
                   </div>
+                  <input type="hidden" name="file_introduction" value="" />
               </div>
 
           <div class="well well-sm">
-            <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i>确认</button>
+            <button type="submit" class="btn btn-primary">确认</button>
             <a class="btn btn-link float-xs-right" href="{{ route('file_shares.index') }}">取消</a>
           </div>
         </form>
@@ -70,34 +71,43 @@
     <script type="text/javascript" src="{{ asset('js/quill.core.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/quill.js') }}"></script>
     <script>
-        var toolbarOptions = [
-            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-            ['blockquote', 'code-block'],
+        // var toolbarOptions = [
+        //     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        //     ['blockquote', 'code-block'],
+        //
+        //     [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        //     [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        //     [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        //     [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        //     [{ 'direction': 'rtl' }],                         // text direction
+        //
+        //     [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        //     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        //
+        //     [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        //     [{ 'font': [] }],
+        //     [{ 'align': [] }],
+        //
+        //     ['image'],['video'],['formula'],
+        //
+        //     ['clean']                                        // remove formatting button
+        //
+        // ];
+        //
+        // var quill = new Quill('#file_introduction', {
+        //     modules: {
+        //         toolbar: toolbarOptions
+        //     },
+        //     theme: 'snow'
+        // });
 
-            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            [{ 'direction': 'rtl' }],                         // text direction
+        var options = {"modules":{"syntax":true,"toolbar":[{"size":[]},{"header":[]},"bold","italic","underline","strike",{"script":"super"},{"script":"sub"},{"color":[]},{"background":[]},"blockquote","code-block",{"list":"ordered"},{"list":"bullet"},{"indent":"-1"},{"indent":"+1"},"direction",{"align":[]},"link","image","video","formula","clean"]},"theme":"snow"}
+        var quill = new Quill("#file_introduction", options);
 
-            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        $('button[type="submit"]').click(function() {
+            var content = document.querySelector('#file_introduction').children[0].innerHTML
+            $('input[name=file_introduction]').val(content)
+        })
 
-            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-            [{ 'font': [] }],
-            [{ 'align': [] }],
-
-            ['image'],['video'],['formula'],
-
-            ['clean']                                        // remove formatting button
-
-        ];
-
-        var quill = new Quill('#editor', {
-            modules: {
-                toolbar: toolbarOptions
-            },
-            theme: 'snow'
-        });
     </script>
     @stop
