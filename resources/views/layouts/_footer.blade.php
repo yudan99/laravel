8bea64014c30f34205e9d8308d406282b97ceaf8
vagrant <vagrant@homestead>
2021-04-10 06:50:46 +0000

    “增加了正文范例和管局连接”

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            Copyright © 2021 广州木鸡信息科技有限公司版权所有 粤ICP备17009382号
+            Copyright © 2021 广州木鸡信息科技有限公司版权所有 <a href="http://beian.miit.gov.cn">粤ICP备17009382号</a>
         </p>
 
         <p class="float-right"><a href="#">联系我们</a></p>
